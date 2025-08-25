011801a14f32720ca937e61ee4a1fb7495206f07
Pedro Henrique <pedro233245@gmail.com>
2025-08-25 02:55:56 -0300

    Atualização Django

--- backend_financas/valores/models.py
+++ backend_financas/valores/models.py
@@ -1,16 +1,10 @@
 from django.db import models
 
-class ValorFixo(models.Model):
-    valor = models.DecimalField(max_digits=10, decimal_places=2)
-    criado_em = models.DateTimeField(auto_now_add=True)
+class Topic(models.Model):
+    """Um assunto sobre o qual o usuario esta aprendendo"""
+    text= models.CharField(max_length=200)
+    date_added = models.DateField(auto_now_add=True)
 
     def __str__(self):
-        return f"Fixo: R$ {self.valor}"
-
-
-class ValorAvulso(models.Model):
-    valor = models.DecimalField(max_digits=10, decimal_places=2)
-    criado_em = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"Avulso: R$ {self.valor}"
+        """Devolve uma representação em string do modelo """
+        return self.text
